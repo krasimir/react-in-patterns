c10e913015caab9e6a6009c3cd4fc27d69dd62ee
Krasimir Tsonev <krasimir@outset.ws>
2018-03-29 07:18:47 +0300

    Adding a redux example app

--- patterns/redux/README.md
+++ patterns/redux/README.md
@@ -1,2 +1,4 @@
 ## [React in patterns](../../README.md) / Redux
 
+[Redux](https://redux.js.org/) is a library that acts as a state container and helps managing your application data flow. It was introduced back in 2015 at ReactEurope conference ([video](https://www.youtube.com/watch?v=xsSnOQynTHs)) by [Dan Abramov](https://twitter.com/dan_abramov). It is inspired by the [Flux architecture](https://github.com/krasimir/react-in-patterns/tree/master/patterns/flux) and has a lot in common with it. In this section we will create a small counter app using Redux alongside React.
+
---
+++ patterns/redux/src/app.jsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Provider, connect } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+
+// Action creators
+
+const ADD = 'ADD';
+const SUBTRACT = 'SUBTRACT';
+const CHANGE_VISIBILITY = 'CHANGE_VISIBILITY';
+
+const add = () => ({ type: ADD });
+const subtract = () => ({ type: SUBTRACT });
+const changeVisibility = visible => ({ type: CHANGE_VISIBILITY, visible });
+
+// Reducers
+
+const initialState = {
+  counter: {
+    value: 0
+  },
+  visible: true
+};
+const counterReducer = function (state = initialState.counter, action) {
+  if (action.type === ADD) {
+    return { value: state.value + 1 };
+  } else if (action.type === SUBTRACT) {
+    return { value: state.value - 1 };
+  }
+  return state;
+};
+const visibilityReducer = function (state = initialState.visible, action) {
+  if (action.type === CHANGE_VISIBILITY) {
+    return action.visible;
+  }
+  return state;
+};
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  visible: visibilityReducer
+});
+
+// Selectors
+
+const getCounterValue = state => state.counter.value;
+const getVisibility = state => state.visible;
+
+// Store creation
+
+const store = createStore(rootReducer);
+
+// React components
+
+function Counter({ value, add, subtract }) {
+  return (
+    <div>
+      <p>Value: { value }</p>
+      <button onClick={ add }>Add</button>
+      <button onClick={ subtract }>Subtract</button>
+    </div>
+  );
+}
+const CounterConnected = connect(
+  state => ({
+    value: getCounterValue(state)
+  }),
+  dispatch => ({
+    add: () => dispatch(add()),
+    subtract: () => dispatch(subtract())
+  })
+)(Counter);
+
+function Visibility({ changeVisibility }) {
+  return (
+    <div>
+      <button onClick={ () => changeVisibility(true) }>Visible</button>
+      <button onClick={ () => changeVisibility(false) }>Hidden</button>
+    </div>
+  );
+}
+const VisibilityConnected = connect(
+  null,
+  dispatch => ({
+    changeVisibility: value => dispatch(changeVisibility(value))
+  })
+)(Visibility);
+
+function App({ visible }) {
+  return (
+    <div>
+      <VisibilityConnected />
+      { visible && <CounterConnected /> }
+    </div>
+  );
+}
+const AppConnected = connect(
+  state => ({
+    visible: getVisibility(state)
+  })
+)(App);
+
+ReactDOM.render(<Provider store={ store }><AppConnected /></Provider>, document.querySelector('#container'));
